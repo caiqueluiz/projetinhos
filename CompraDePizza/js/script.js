482e9ba6aefc1e263308a9104d7291c605420d89
Caique Luiz <caiquexh@gmail.com>
2022-09-27 12:32:13 -0300

    adicionando botoes no modal

--- CompraDePizza/js/script.js
+++ CompraDePizza/js/script.js
@@ -1,3 +1,7 @@
+let modalQt = 1
+
+
+// Listagem das pizzas
 pizzaJson.map((item, index) => {
     const pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
 
@@ -7,13 +11,27 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
 
+
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault()
-
         let key = e.target.closest('.pizza-item').getAttribute('data-key')
+        modalQt = 1
 
         document.querySelector('.pizzaInfo h1').innerHTML = pizzaJson[key].name
         document.querySelector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+        document.querySelector('.pizzaBig img').src = pizzaJson[key].img
+        document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
+        document.querySelector('.pizzaInfo--size.selected').classList.remove('selected')
+
+        document.querySelectorAll('.pizzaInfo--size').forEach((size, sizeIndex)=> {
+            if(sizeIndex == 2) {
+                size.classList.add('selected')
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+        })
+
+        document.querySelector('.pizzaInfo--qt').innerHTML = modalQt
+
 
         document.querySelector('.pizzaWindowArea').style.opacity = 0
         document.querySelector('.pizzaWindowArea').style.display = 'flex'
@@ -26,3 +44,14 @@ pizzaJson.map((item, index) => {
     document.querySelector('.pizza-area').append(pizzaItem)
 })
 
+// Eventos do Modal 
+function closeModal() {
+    document.querySelector('.pizzaWindowArea').style.opacity = 0
+    setTimeout(()=> {
+        document.querySelector('.pizzaWindowArea').style.display = 'none'
+    }, 500)
+}
+
+document.querySelectorAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=> {
+    item.addEventListener('click', closeModal)
+})

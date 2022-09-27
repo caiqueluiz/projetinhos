0c90eaeff553b4f5662d28a8ddc8990851ef83a3
Caique Luiz <caiquexh@gmail.com>
2022-09-27 16:58:58 -0300

    projeto finalizado, treinando JS

--- CompraDePizza/js/script.js
+++ CompraDePizza/js/script.js
@@ -103,7 +103,21 @@ document.querySelector('.pizzaInfo--addButton').addEventListener('click', ()=> {
 
 })
 
+document.querySelector('.menu-openner').addEventListener('click', ()=> {
+    if(cart.length > 0 ) {
+        document.querySelector('aside').style.left = '0'
+    }
+})
+
+document.querySelector('.menu-closer').addEventListener('click', ()=>{
+    document.querySelector('aside').style.left = '100vw'
+})
+
 function updateCart() {
+
+    document.querySelector('.menu-openner span').innerHTML = cart.length
+
+
     if(cart.length > 0) {
         document.querySelector('aside').classList.add('show')
         document.querySelector('.cart').innerHTML = ''
@@ -170,5 +184,6 @@ function updateCart() {
 
     } else {
         document.querySelector('aside').classList.remove('show')
+        document.querySelector('aside').style.left = '100vw'
     }
 }

f37757f9e42717ef64700c8b4c4a05f5bbd2adcf
Caique Luiz <caiquexh@gmail.com>
2022-09-26 22:08:10 -0300

    atualizando algumas coisinhas

--- CompraDePizza/js/script.js
+++ CompraDePizza/js/script.js
@@ -1,6 +1,7 @@
 pizzaJson.map((item, index) => {
     const pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
 
+    pizzaItem.setAttribute('data-key', index)
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
@@ -8,6 +9,12 @@ pizzaJson.map((item, index) => {
 
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault()
+
+        let key = e.target.closest('.pizza-item').getAttribute('data-key')
+
+        document.querySelector('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+        document.querySelector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+
         document.querySelector('.pizzaWindowArea').style.opacity = 0
         document.querySelector('.pizzaWindowArea').style.display = 'flex'
         setTimeout(()=> {
